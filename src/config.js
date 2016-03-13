168fc3d473a09991146bc4b6b8ed632c71a728b5
Paul Sajna <paulsajna@gmail.com>
2016-03-13 06:34:21 -0700

    Revert "use @ksaredfx's elastic search endpoint"
    This reverts commit efe52c438c60d24113c08337fce5314a2c5a18ae.

--- src/config.js
+++ src/config.js
@@ -7,6 +7,6 @@ export default {
         url: cfenv.url || `http://localhost:${(cfenv.port || 4000)}`
     },
     elasticsearch: {
-        host: 'http://keyser.reelgoodapp.com:9400'
+        host: 'https://search-hackvictoria-l5svhzy24drpsgt2jshws4zzei.us-west-2.es.amazonaws.com'
     }
 }
