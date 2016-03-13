d3c707f816ee58ab201c88516806af321d8a44cb
Luigi Poole <luigipoole@outlook.com>
2016-03-12 20:32:34 -0800

    fix

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ import cfenv from 'cfenv'
 
 export default {
     koa: {
-        port: cfenv.port || 4000
+        port: cfenv.port || 4000,
         url: cfenv.url || `http://localhost:${(cfenv.port || 4000)}`
     },
     elasticsearch: {
