b7fd29b525ef4939ed0c076286accd43aa8469d7
Luigi Poole <luigipoole@outlook.com>
2016-03-12 20:29:50 -0800

    use cfenv

--- src/config.js
+++ src/config.js
@@ -1,6 +1,10 @@
+import cfenv from 'cfenv'
+
+
 export default {
     koa: {
-        port: 4000
+        port: cfenv.port || 4000
+        url: cfenv.url || `http://localhost:${(cfenv.port || 4000)}`
     },
     elasticsearch: {
         host: 'https://search-hackvictoria-l5svhzy24drpsgt2jshws4zzei.us-west-2.es.amazonaws.com'
