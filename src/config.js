6c9991704af1348ec359bd37fabc669eff3abf30
Paul Sajna <paulsajna@gmail.com>
2016-03-13 06:49:36 -0700

    fix bluemix shit

--- src/config.js
+++ src/config.js
@@ -1,12 +1,13 @@
 import cfenv from 'cfenv'
 
+var appEnv = cfenv.getAppEnv();
 
 export default {
     koa: {
-        port: cfenv.port || 4000,
-        url: cfenv.url || `http://localhost:${(cfenv.port || 4000)}`
+        port: appEnv.port || 4000,
+        ip: '0.0.0.0' || 'localhost'
     },
     elasticsearch: {
-        host: 'https://search-hackvictoria-l5svhzy24drpsgt2jshws4zzei.us-west-2.es.amazonaws.com'
+        host: 'http://keyser.reelgoodapp.com:9400/'
     }
 }
